f276809543ff7adb2db3ed5b50520e65e6f8230c
unknown <santosh.maharjan1@gmail.com>
2022-02-07 18:10:27 -0600

    cup and mobile added

---
+++ src/training/day1/Mobile.java
@@ -0,0 +1,22 @@
+package training.day1;
+
+public class Mobile {
+    // states
+
+    // Brand
+    // type
+    // color
+    // Memory
+    // camera
+    // storage
+
+    // Behavior
+
+    // call
+    // capturing photo, video
+    // storage
+    // use internet
+    // digital transaction
+    // navigation
+
+}
---
+++ src/training/day1/cup.java
@@ -0,0 +1,16 @@
+package training.day1;
+
+public class cup {
+    // states
+
+    // color
+    // size
+    // type
+    // material
+
+    // Behavior
+
+    // retain water
+    // cool the hot water
+
+}
